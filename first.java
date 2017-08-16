1cab6c1f770973a6568da8eb9867930066562afc
chiranshu11 <ca1897@gmail.com>
2017-08-16 09:27:57 +0530

    First java code

---
+++ first.java
@@ -0,0 +1,11 @@
+class first
+	{
+	public static void main(String[] args)
+	//main and args are identifiers made by users
+	//System and strings are built in classes made by user
+		{
+		System.out.print(" hi1");
+		System.out.println(" hi2");
+		System.out.print(" hi3");
+		}
+	}
