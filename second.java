23678783305047579f32258d9818022839be443e
chiranshu11 <ca1897@gmail.com>
2017-08-17 23:41:49 +0530

    java code

---
+++ second.java
@@ -0,0 +1,23 @@
+class first
+	{
+	public static void main(String args[])
+		{
+		System.out.println(" hi1 \n hi");
+		// for new line
+		System.out.println(" hi2 \t hi");
+		// for space
+		System.out.println(" hi3 \\ j");
+		// for \
+		System.out.println(" hi4 \"-" ");
+		// for double quote( ")
+		System.out.println(" hi5 \'-' ");
+		// for single quote( ')
+		System.out.println(" hi6 \f ");
+		// for fomfeed sign
+		System.out.println(" hi7 \r b");
+		// first compiler will print normally and when it reaches\r it will jump to start of line and start printing again the string which is written after \reaches\r
+		//here it will print bi
+		System.out.println(" hi8\b");
+		// to remove string from left side of it like now it will print hi not hi8
+		}
+	}
